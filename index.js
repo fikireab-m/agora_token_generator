1195209a4b40585e0d6265a62b5387491b55be15
fikireab-m <fikireab.meku@gmail.com>
2023-09-11 12:54:56 +0300

    testing 1

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use('/', function (req, res) {
+    res.status(201).send('Agora Token generator');
+});
+
 app.use("/api", require("./api/generate"));
 
-app.use('*', function (req, res) {
-    res.status(404).send('Not found');
-});
+
 // Start the server
 app.listen(5000, () => {
     console.log("Server is running on port: 5000");
